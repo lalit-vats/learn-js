46b080e362edcd073a676a5e4d25aff7a9feea21
Lalit Vats <lalitvats863@gmail.com>
2024-07-13 12:27:50 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 54367
+let accountEmail = 'lalit@google.com'
+var accountPassword = '12345'
+accountCity = 'Delhi'
+
+//accountId = 2  // you can not reassign the value in to "const" keyword
+
+accountEmail = 'lalit123@gmail.com'
+
+accountPassword = '123456'
+accountCity = 'Gurgaon'
+
+console.log(accountId)
+
+console.table([accountEmail,accountPassword,accountCity])
+
+
+//4th video completed---here

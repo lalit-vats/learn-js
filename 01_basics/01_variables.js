6e3a60b921e9b8f1814b03674b2ee419d9ed113c
Lalit Vats <lalitvats863@gmail.com>
2024-07-31 12:23:50 +0530

    1st day assignment done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,18 +1,53 @@
-const accountId = 54367
-let accountEmail = 'lalit@google.com'
-var accountPassword = '12345'
-accountCity = 'Delhi'
+// const accountId = 54367
+// let accountEmail = 'lalit@google.com'
+// var accountPassword = '12345'
+// accountCity = 'Delhi'
 
 //accountId = 2  // you can not reassign the value in to "const" keyword
 
-accountEmail = 'lalit123@gmail.com'
+// accountEmail = 'lalit123@gmail.com'
 
-accountPassword = '123456'
-accountCity = 'Gurgaon'
+// accountPassword = '123456'
+// accountCity = 'Gurgaon'
 
-console.log(accountId)
+// console.log(accountId)
 
-console.table([accountEmail,accountPassword,accountCity])
+// console.table([accountEmail,accountPassword,accountCity])
 
 
 //4th video completed---here
+
+//js test here*****
+
+//var decalration
+// var lalit = 26
+
+//console.log(lalit);
+
+//let decalration
+
+// let ram = 'vats'
+
+// console.log(ram);
+//const decalration
+
+// const Shiv = true
+
+// console.log(Shiv);
+
+
+// Declaring all data type variables
+
+var Ramu = "Supervisor"
+let Lalu = "HawalDar"
+const Shabana =  "Beauty"
+let num = 1
+let arr = [1,2,3,4]
+let obj = {name:"Lalit",age:26}
+
+//console.table([ Ramu,Lalu,Shabana,num,arr,obj]);
+
+//console.log(typeof(Ramu))
+//console.log(typeof(num))
+//console.log(typeof(arr))
+console.log(typeof(obj))
